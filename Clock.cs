578b6b14dbfadfd296f6d5af5e694f175bb9ba85
sturs23 <sturs23@gmail.com>
2019-07-14 11:41:25 +0300

    Add files via upload

---
+++ Clock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Clock
+    {
+        private static Clock INSTANCE;
+        private static Object lockobject = new object();
+
+        private Clock()
+        {
+                
+        }
+
+        public static Clock GetInstance()
+        {
+            if (INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+
+            lock (lockobject)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Clock();
+                }
+            }
+
+            return INSTANCE;
+        }
+
+        public DateTime GetTime()
+        {
+            return (DateTime.Now);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock a = Clock.GetInstance();
+            Clock b = Clock.GetInstance();
+            Clock c = Clock.GetInstance();
+
+            Console.WriteLine(a.GetTime());
+            Console.WriteLine(b.GetTime());
+            Console.WriteLine(c.GetTime());
+        }
+    }
+}
